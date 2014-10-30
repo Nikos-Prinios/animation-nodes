e53ea87ac5d1b0104ca48d9fcb55a8cc49fe4dd8
Nikos <nikos@habanita.org>
2014-10-30 16:58:48 +0100

    Update mn_track_to.py
    more explicit variables & ouput a real vector

--- nodes/vector/mn_track_to.py
+++ nodes/vector/mn_track_to.py
@@ -1,28 +1,4 @@
-import bpy, math
-from bpy.types import Node
-from mn_node_base import AnimationNode
-from mn_execution import nodePropertyChanged, allowCompiling, forbidCompiling
-from mn_utils import *
-
-def updateNode(node, context):
-	nodeTreeChanged()
-
-class mn_TrackTo(Node, AnimationNode):
-	bl_idname = "mn_track_to"
-	bl_label = "Track to"
-	node_category = "Vector"
-
-	axis = bpy.props.BoolVectorProperty(update = nodePropertyChanged)
-
-	def init(self, context):
-		forbidCompiling()
-		self.inputs.new("mn_ObjectSocket", "input")
-		self.inputs.new("mn_ObjectSocket", "track")
-		self.outputs.new("mn_VectorSocket", "output")
-		self.inputs.new("mn_FloatSocket", "Offset")
-		allowCompiling()
-
-	def draw_buttons(self, context, layout):
+def draw_buttons(self, context, layout):
 		col = layout.column(align = True)
 			
 		row = col.row(align = True)
@@ -32,18 +8,21 @@ class mn_TrackTo(Node, AnimationNode):
 		row.prop(self, "axis", index = 2, text = "z")
 
 
-	def angle(self,a,b,c,d,i,offset):
+	def angle(self,target_x,object_x,target_y,object_y,i,offset):
 
-		if c < d:
+		if target_y < object_y:
 			i *= -1
-		
-		result = (math.acos((a-b)/math.sqrt((a-b)**2 + (c-d)**2)) * i) + offset
+			
+		result = (math.acos((target_x-object_x)/math.sqrt((target_x-object_x)**2 + (target_y-object_y)**2)) * i) + offset
 		return result
 
 	def execute(self, input):
-		output = {}
+		
 		offset = input["Offset"]
 		obj = input["input"]
+
+		output = {}
+		result = [0.0,0.0,0.0]
 
 		if obj is None:
 			return ""
@@ -61,18 +40,12 @@ class mn_TrackTo(Node, AnimationNode):
 		track_y = track.location[1]
 		track_z = track.location[2]
 
-		X = 0.0
-		Y = 0.0
-		Z = 0.0
-
 		if self.axis[0]:
-			X = self.angle(track_y, obj_y, track_z, obj_z, 1, offset)
+			result[0] = self.angle(track_y, obj_y, track_z, obj_z, 1, offset)
 		if self.axis[1]:
-			Y = self.angle(track_x, obj_x, track_z, obj_z, -1, offset)
+			result[1] = self.angle(track_x, obj_x, track_z, obj_z, -1, offset)
 		if self.axis[2]:
-			Z = self.angle(track_x, obj_x, track_y, obj_y, 1, offset)
+			result[2] = self.angle(track_x, obj_x, track_y, obj_y, 1, offset)
 		
-		output["output"] = [X,Y,Z]
+		output["output"] = result
 		return output
-
-		
