b280e409a62d1fd16d35a9dc46a4f7cb8a870b8a
Nikos <nikos@habanita.org>
2014-10-30 12:57:37 +0100

    Update mn_track_to.py
    Add an offset input socket for easy adjustment

--- nodes/vector/mn_track_to.py
+++ nodes/vector/mn_track_to.py
@@ -19,6 +19,7 @@ class mn_TrackTo(Node, AnimationNode):
 		self.inputs.new("mn_ObjectSocket", "input")
 		self.inputs.new("mn_ObjectSocket", "track")
 		self.outputs.new("mn_VectorSocket", "output")
+		self.inputs.new("mn_FloatSocket", "Offset")
 		allowCompiling()
 
 	def draw_buttons(self, context, layout):
@@ -31,17 +32,17 @@ class mn_TrackTo(Node, AnimationNode):
 		row.prop(self, "axis", index = 2, text = "z")
 
 
-	def angle(self,a,b,c,d,i):
+	def angle(self,a,b,c,d,i,offset):
 
 		if c < d:
 			i *= -1
 		
-		result = math.acos((a-b)/math.sqrt((a-b)**2 + (c-d)**2)) * i
+		result = (math.acos((a-b)/math.sqrt((a-b)**2 + (c-d)**2)) * i) + offset
 		return result
 
 	def execute(self, input):
 		output = {}
-		
+		offset = input["Offset"]
 		obj = input["input"]
 
 		if obj is None:
@@ -65,11 +66,11 @@ class mn_TrackTo(Node, AnimationNode):
 		Z = 0.0
 
 		if self.axis[0]:
-			X = self.angle(track_y, obj_y, track_z, obj_z,1)
+			X = self.angle(track_y, obj_y, track_z, obj_z, 1, offset)
 		if self.axis[1]:
-			Y = self.angle(track_x, obj_x, track_z, obj_z,-1)
+			Y = self.angle(track_x, obj_x, track_z, obj_z, -1, offset)
 		if self.axis[2]:
-			Z = self.angle(track_x, obj_x, track_y, obj_y,1)
+			Z = self.angle(track_x, obj_x, track_y, obj_y, 1, offset)
 		
 		output["output"] = [X,Y,Z]
 		return output
