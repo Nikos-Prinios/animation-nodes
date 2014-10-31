20270dfca87f4486e6a90671efba2976455ee77e
Nikos <nikos@habanita.org>
2014-10-31 16:59:57 +0100

    Update mn_shapekeys.py
    Working menu but with shapekeys of the whole scene.

--- nodes/object/mn_shapekeys.py
+++ nodes/object/mn_shapekeys.py
@@ -3,7 +3,6 @@ from bpy.types import Node
 from mn_node_base import AnimationNode
 from mn_execution import nodeTreeChanged, allowCompiling, forbidCompiling
 
-
 def updateNode(node, context):
 	nodeTreeChanged()
 
@@ -11,21 +10,36 @@ class mn_ShapeKeys(Node, AnimationNode):
 	bl_idname = "mn_ShapeKeys"
 	bl_label = "Object Shape Keys"
 
+	def get_shapes_menu_items(self, context):
+		shapes_menu_items = []
+		for s in bpy.data.shape_keys:
+			for sh in s.key_blocks:
+				shapes_menu_items.append((sh.name, sh.name, ""))
+		if len(shapes_menu_items) == 0: shapes_menu_items.append(("NONE", "NONE", ""))
+		return shapes_menu_items
+
+	shapes_menu = bpy.props.EnumProperty(name = "Shape Keys Menu", items = get_shapes_menu_items)
+	
 	def init(self, context):
 		forbidCompiling()
 		self.inputs.new("mn_ObjectSocket", "Object")
-		self.inputs.new("mn_StringSocket", "Shape key")
 		self.inputs.new("mn_FloatSocket", "Value")
 		allowCompiling()
+	
+	def draw_buttons(self, context, layout):
+		col = layout.column(align = True)
+		layout.prop(self, "shapes_menu")
 
 	def execute(self, input):
-		object = input["Object"]
-		Shape = input["Shape key"]
+		obj = input["Object"]
+		if obj is None:
+			return ""
+		Shape = self.shapes_menu
 		Value = input["Value"]
-		obj_shape_keys = object.data.shape_keys.key_blocks
-		if Shape in obj_shape_keys:
-			object.data.shape_keys.key_blocks[Shape].value=Value
-
-		if object is None:
+		try:
+			obj_shape_keys = obj.data.shape_keys.key_blocks
+			if Shape in obj_shape_keys:
+				obj.data.shape_keys.key_blocks[Shape].value=Value
+		except:
 			return ""
 
